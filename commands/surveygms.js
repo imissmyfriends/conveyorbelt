ac95c9252b66c7abd55990e6bf541a743bb67ef0
prtksxna <prtksxna@gmail.com>
2022-06-07 13:59:04 +0530

    feat: check if YYP file exists

--- commands/surveygms.js
+++ commands/surveygms.js
@@ -1,5 +1,32 @@
+const Listr = require('listr');
+const fs = require('fs');
+var glob = require("glob")
+
 function surveyGMS() {
-  console.log(111);
+  const tasks = new Listr([
+    {
+      title: "Check YYP file exists",
+      task: checkYYPFileExists
+    }
+  ]);
+  tasks.run().catch(err => {
+    console.error(err);
+  });
+}
+
+function checkYYPFileExists() {
+  var checkProject = new Promise((resolve, reject) => {
+    glob("*.yyp", function (error, files) {
+      if (error != null) {
+        reject(error);
+      } else if (files.length == 0) {
+        reject(new Error('No .yyp file'));
+      } else {
+        resolve(true);
+      }
+    });
+  })
+  return checkProject;
 }
 
 module.exports = surveyGMS;
