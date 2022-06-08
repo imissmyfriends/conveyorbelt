735dbd430a0491cbb65066a448b0920dc3a5c41e
prtksxna <prtksxna@gmail.com>
2022-06-08 13:10:00 +0530

    feat: get sprite details

--- commands/surveygms.js
+++ commands/surveygms.js
@@ -7,7 +7,54 @@ function surveyGMS() {
     {
       title: "Check YYP file exists",
       task: checkYYPFileExists
+    },
+    {
+      title: "Check sprites directory exists",
+      task: checkSpritesDirExists
+    },
+    {
+      title: "Get sprites",
+      task: getSpriteDirectories
+    },
+    {
+      title: "Look into sprites",
+      task: function (ctx) {
+        ctx.spriteDetails = {};
+        var subTasks = ctx.files.map(file => {
+          return {
+            title: `Looking up ${file}...`,
+            task: function (ctx) {
+              var readSprite = new Promise((resolve, reject) => {
+                fs.readFile(file, 'utf8', (err, data) => {
+                  if (err) {
+                    reject(new Error(`Error reading file from disk: ${err}`));
+                  } else {
+                    var sprite = getSpriteData(data);
+                    ctx.spriteDetails[sprite.name] = getSpriteDetails(sprite);
+                    resolve(ctx);
+                  }
+                });
+              });
+              return readSprite;
+            }
+          };
+        })
+        return new Listr(subTasks)
+      }
+    },
+    {
+      title: "Show sprite info",
+      task: function (ctx) {
+        console.log(ctx.spriteDetails);
+      }
     }
+    // Check sprites directory exists
+    // List sprites
+    // Check each sprite
+      // Load JSON
+      // See filename
+      // See layer filename
+      // Check if it is an animation
   ]);
   tasks.run().catch(err => {
     console.error(err);
@@ -15,18 +62,59 @@ function surveyGMS() {
 }
 
 function checkYYPFileExists() {
+  return getGlobPromise("*.yyp", "No .yyp file");
+}
+
+function checkSpritesDirExists() {
+  return getGlobPromise("sprites/", "No sprites directory");
+}
+
+function getSpriteDirectories(ctx) {
+    return getGlobPromise("sprites/*/*.yy", "No sprites found").then(function (files) {
+      ctx.files = files;
+    });
+}
+
+function getGlobPromise(globMatch, errorText) {
   var checkProject = new Promise((resolve, reject) => {
-    glob("*.yyp", function (error, files) {
+    glob(globMatch, function (error, files) {
       if (error != null) {
         reject(error);
       } else if (files.length == 0) {
-        reject(new Error('No .yyp file'));
+        reject(new Error(errorText));
       } else {
-        resolve(true);
+        resolve(files);
       }
     });
   })
   return checkProject;
+}
+
+function getSpriteData(data) {
+  // Remove trailing commas
+  let regex = /\,(?!\s*?[\{\[\"\'\w])/g;
+  let correctData = data.replace(regex, '');
+
+  // parse JSON string to JSON object
+  return JSON.parse(correctData);
+}
+
+function getSpriteDetails(sprite) {
+  if (sprite.frames.length === 1 ) {
+    return {
+      imgName: sprite.frames[0].compositeImage.FrameId.name,
+      layerName: sprite.layers[0].name
+    };
+  } else {
+    var imgNames = sprite.frames.map(frame => {
+      return frame.compositeImage.FrameId.name;
+    });
+
+    return {
+      imgNames,
+      layerName: sprite.layers[0].name
+    }
+  }
 }
 
 module.exports = surveyGMS;
