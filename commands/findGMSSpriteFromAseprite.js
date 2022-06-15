6538e515b8918418e9ab491618a7b5b5c44758a0
prtksxna <prtksxna@gmail.com>
2022-06-15 16:38:42 +0530

    docs(findGMSSpriteFromAseprite): add jsdoc

--- commands/findGMSSpriteFromAseprite.js
+++ commands/findGMSSpriteFromAseprite.js
@@ -31,18 +31,23 @@ function findGMSSpriteFromAseprite(filePath, ctx, observer) {
           importSingleGMSSprite(pngs, ctx, observer);
         } else {
           importAnimationGMSSprite(pngs, ctx, observer);
-//          observer.next(pngs);
         }
       });
     });
-
-    //console.log(files);
-    //observer.next(globMatch);
-    //observer.next(files[0]);
   });
 }
 module.exports = findGMSSpriteFromAseprite;
 
+/**
+ * Find all PNGs that could be associated with a sprite (inside a gived folder)
+ * This is done by appending a `-` to the name and finding all PNGs. This returns
+ * all frames in the case of animations and just one file for other kinds of 
+ * sprites.
+ * 
+ * @param {string} spriteName 
+ * @param {string} dir 
+ * @returns {Promise}
+ */
 function getPNGsForSprite(spriteName, dir) {
   var promise = new Promise((resolve, reject) => {
     glob(`${dir}/${spriteName}-*.png`, (error, pngs) => {
@@ -53,9 +58,14 @@ function getPNGsForSprite(spriteName, dir) {
   return promise;
 }
 
-// This function might return more than what is affected
-// but we check for difference before making changes to GMS anyway
-// So its ok
+/**
+ * Creates a list of sprites (whether existing or not) that would
+ * need updating. This is done solely on the basis of that every PNG
+ * would have a corresponding sprite.
+ * 
+ * @param {Array} files - list of PNG files
+ * @returns {Array} - list of sprites that might need updating
+ */
 function getAffectedSprites(files) {
   files = files.map((file) => {
     return path.basename(file,'.png');
@@ -63,20 +73,39 @@ function getAffectedSprites(files) {
   files = files.map((file) => {
     return file.split('-')[0];
   });
-  files = files.filter(onlyUnique); 
-  return files;
+  let sprites = files.filter(onlyUnique); 
+  return sprites;
 }
 
+/**
+ * Returns a glob to find PNG files that were created by this aseprite file.
+ * If the name of the aseprite file is a prefix for other files (for eg UIBubble
+ * and UIBubblePointer) then both will be matched.
+ * 
+ * @param {String} filePath - path to asperite file
+ * @param {Object} ctx 
+ * @returns {String} Glob for matching PNG files
+ */
 function getPNGGlobFromAseprite(filePath, ctx) {
   let name = path.basename(filePath, '.aseprite');
   let dir = path.dirname(filePath);
   return `${dir}/${ctx.PREFIX}${name}*.png`;
 }
 
+/**
+ * Array filter to return only unique values
+ */
 function onlyUnique(value, index, self) {
   return self.indexOf(value) === index;
 }
 
+/**
+ * Imports a single PNG file into GMS
+ * 
+ * @param {Array} files - array contaiting a single filePath
+ * @param {Object} ctx 
+ * @param {Object} observer 
+ */
 function importSingleGMSSprite(files, ctx, observer) {
   let pngName = path.basename(files[0], '.png');
   let spriteName = pngName.split('-')[0];
@@ -91,6 +120,13 @@ function importSingleGMSSprite(files, ctx, observer) {
   );
 }
 
+/**
+ * Imports an array of animation PNG file into GMS
+ * 
+ * @param {Array} files - array contaiting a PNG filePaths
+ * @param {Object} ctx 
+ * @param {Object} observer 
+ */
 function importAnimationGMSSprite(files, ctx, observer) {
   let pngName = path.basename(files[0], '.png');
   let spriteName = pngName.split('-')[0];
@@ -107,6 +143,16 @@ function importAnimationGMSSprite(files, ctx, observer) {
   })
 }
 
+/**
+ * Given the spriteName and optionally and index, this function looks up the 
+ * spriteDetails context and returns the path of the image and layer PNGs that
+ * need to be updated
+ * 
+ * @param {Object} ctx 
+ * @param {String} spriteName 
+ * @param {Number} index 
+ * @returns {Object}
+ */
 function getSpritePaths(ctx, spriteName, index) {
   var imgName, layerImgName;
   if (index == undefined) {
@@ -135,6 +181,10 @@ function getSpritePaths(ctx, spriteName, index) {
   }
 }
 
+/**
+ * Compares to PNGs and if they aren't the same copies the Aseprite export
+ * into GMS
+ */
 function compareAndCopy(pngPath, spriteImgPath, spriteLayerImgPath, spriteName, observer) {
   var pngBuf = fs.readFileSync(pngPath);
   var gmsBuf = fs.readFileSync(spriteImgPath);
@@ -147,6 +197,9 @@ function compareAndCopy(pngPath, spriteImgPath, spriteLayerImgPath, spriteName, 
   }
 }
 
+/**
+ * Copies file to location and sends an update on the observer.
+ */
 function copyFiles(from, to, spriteName, observer) {
   fs.copyFile(from, to, (error) => {
     if (error) throw new Error(error);
